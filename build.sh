bbdb96e02a24bbbab44aae629dcd85711308bb0f
BartoszBugla <buglabartosz@gmail.com>
2026-06-01 21:29:14 +0200

    Remove --no-input from tailwind commands in build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 
 # Zainstaluj i zbuduj TailwindCSS
-python manage.py tailwind install --no-input
-python manage.py tailwind build --no-input
+python manage.py tailwind install
+python manage.py tailwind build
 
 # Skompiluj pliki statyczne
 python manage.py collectstatic --no-input
